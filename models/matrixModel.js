724173a173751cd667d6c6fb30cff37db9e4ffa8
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-08-08 09:38:56 +0200

    added methods that move numbers to left and right

--- models/matrixModel.js
+++ models/matrixModel.js
@@ -15,7 +15,7 @@ function MatrixModel() {
         return instance;
     }
 
-    this.displayDigitByKeyPress();
+    this.initGame();
 }
 
 
@@ -34,11 +34,55 @@ MatrixModel.prototype.randomNumber = function () {
     return Math.random() < 0.8 ? '2' : '4';
 }
 
-MatrixModel.prototype.displayDigitByKeyPress = function (code) {
+MatrixModel.prototype.initGame = function () {
     this.attributes.grid[this.randomPlace()][this.randomPlace(0, this.generalSize / 2.5)] = this.randomNumber();
     this.attributes.grid[this.randomPlace()][this.randomPlace(this.generalSize / 2 , this.generalSize )] = this.randomNumber();
 
     this.publish('changeData');
+}
+
+MatrixModel.prototype.displayDigitByKeyPress = function (code) {
+    switch (code) {
+        case 'ArrowRight':
+            this.moveToRight();
+            break;
+        case 'ArrowLeft':
+            this.moveToLeft();
+            break;
+        case 'ArrowUp':
+            console.log('up')
+            break;
+        case 'ArrowDown':
+            console.log('down')
+        break;
+    }
+
+    this.attributes.grid[this.randomPlace()][this.randomPlace()] = this.randomNumber();
+    this.attributes.grid[this.randomPlace()][this.randomPlace()] = this.randomNumber();
+  
+    this.publish('changeData');
+}
+
+MatrixModel.prototype.moveToRight = function () {
+    this.attributes.grid.map(function (array) {
+        return array.map(function (elem, i) {
+            if (elem !== '') {
+                let newIndex = i + 1;
+                return array.splice(newIndex, 0, array.splice(i, 1)[0]);
+           }
+        }) 
+    })
+}
+
+MatrixModel.prototype.moveToLeft = function () {
+    this.attributes.grid.map(function (array) {
+        return array.map(function (elem, i) {
+            if (elem !== '') {
+                let newIndex = i - 1;
+                return array.splice(newIndex, 0, array.splice(i, 1)[0])
+           }
+        }) 
+    })
 }
 
 MatrixModel.prototype.startNewGame = function () {
